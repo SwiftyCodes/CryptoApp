9427ae3072624ca4a2cafb8d4386508aaebf66c8
Anurag Kashyap <swiftycodes@gmail.com>
2023-04-29 02:07:39 +0530

    Details Page Added

---
+++ CryptoApp/Extension/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Anudhi on 27/04/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let primaryTextColor = Color("primaryTextColor")
+    let itemBackgroundColor = Color("itemBackgroundColor")
+}
